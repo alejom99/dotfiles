a3dd6161888ba2218fb05265a27365ec346f39f2
Alejandro Mesa <alejom99@gmail.com>
2021-01-09 09:16:34 -0800

    Removed old docker config.

--- docker/shellinit.zsh
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------
-# boot2docker
-# ----------------------------------------------------------------------
-if [ "$(docker-machine status docker-vm)" = "Running" ]
-then
-    eval $(docker-machine env docker-vm)
-fi
