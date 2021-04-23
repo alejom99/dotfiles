cd74f493689bfafb1926168056845f25c915f95e
Alejandro Mesa <alejom99@gmail.com>
2021-04-22 21:19:48 -0700

    Removed old theme.

--- antigen/install.sh
+++ antigen/install.sh
@@ -3,7 +3,4 @@
 echo "Installing antigen ..."
 source $ZSH/bin/antigen.zsh
 
-local REPO_DIR="$(-antigen-get-clone-dir $ANTIGEN_DEFAULT_REPO_URL)/themes"
-cp $ZSH/antigen/alejandro.zsh-theme $REPO_DIR
-
 exit 0
